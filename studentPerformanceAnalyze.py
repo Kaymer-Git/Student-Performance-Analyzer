b4b4ef2c4a08e98765a11f4e3ec77e67fd9f4ce3
StudentPM <studentpm@Administrators-MacBook-Pro-2.local>
2026-09-21 15:24:46 -0700

    second time, not finish yet

--- studentPerformanceAnalyze.py
+++ studentPerformanceAnalyze.py
@@ -37,6 +37,8 @@ def calculate_grade(assignment_average, quiz_average, test_average):
     test_portion = test_average * 4/10
     overall_grade = assignment_portion + quiz_portion + test_portion
     print("Overall grade: " + str(overall_grade))
+    return overall_grade
+overall_grade = float(calculate_grade(assignment_ave, quiz_ave, test_ave))
 
 # create a function to make letter grade
 def letter_grade(overall_grade):
@@ -50,3 +52,65 @@ def letter_grade(overall_grade):
         print("Letter grade: D")
     else:
         print("Letter grade: F")
+
+# showing attendance status
+def attendance_status(attendance):
+    if attendance >= 95:
+        print("Attendance Status: Excellent Attendance")
+    elif attendance >= 90:
+        print("Attendance Status: Good Attendance")
+    elif attendance >= 80:
+        print("Attendance Status: Attendance Warning")
+    else:
+        print("Attendance Status: Poor Attendance")
+
+# Function for Missing Assignment Status
+def assignment_status(missing_assignments):
+    if missing_assignments == 0:
+        print("Missing Assignment Status: Excellent")
+    elif missing_assignments <= 2:
+        print("Missing Assignment Status: Good")
+    elif missing_assignments  <= 4:
+        print("Missing Assignment Status: Warning")
+    else:
+        print("Missing Assignment Status: Critical")
+
+# Nested Conditional: Academic Eligibility
+def check_eligibility(overall_grade, attendance, missing_assignments):
+    if overall_grade >= 70:
+        if attendance >= 90:
+            if missing_assignments <= 2:
+                print("Academic Eligibility: ELIGIBLE")
+                print("Student passed all three requirements.")
+            else:
+                print("Academic Eligibility: NOT ELIGIBLE")
+                print("Reason: Too many missing assignments.")
+        else:
+            print("Academic Eligibility: NOT ELIGIBLE")
+            print("Reason: Attendance is too low.")
+    else:
+        print("Academic Eligibility: NOT ELIGIBLE")
+        print("Reason: Overall grade is too low.")
+
+# Nested Conditional: High Honors
+def check_high_honors(overall_grade, attendance, missing_assignments):
+    if overall_grade >= 90:
+        if attendance>= 95:
+            if missing_assignments == 0:
+                print("High Honors: YES")
+            else:
+                print("High Honors: NO")
+                print("Reason: Student has missing assignments.")
+        else:
+            print("High Honors: NO")
+            print("Reason: Attendance requirement not met.")
+    else:
+        print("High Honors: NO")
+        print("Reason: Grade requirement not met.")
+
+# Check for good standing
+def check_good_standing(overall_grade, attendance):
+    if overall_grade >= 70 and attendance >= 90:
+        print("Good Standing: YES")
+    else:
+        print("Good Standing: NO")
